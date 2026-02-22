31c042e2c7e277874171afe32e63ae601d83e5f6
Tomoyuki KOYAMA <2428176+tomoyk@users.noreply.github.com>
2026-02-22 17:25:32 +0900

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -12,15 +12,19 @@ echo -n > ~/hosts.tsv
 echo "# BEGIN_GEN_HOSTS" >> ~/hosts.tsv
 sshpass -p ${ESXI_PASSWORD} ssh ${ESXI_USER}@${ESXI_HOST} 'vim-cmd vmsvc/getallvms' | while read line
 do
-	# echo $line
-	vmid=$(echo $line | awk '{print $1}')
-	vmname=$(echo $line | awk '{print $2}')
-	cmd="vim-cmd vmsvc/get.summary $vmid | grep ipAddress"
-	# echo "cmd: $cmd"
-	ipaddr=$(sshpass -p ${ESXI_PASSWORD} ssh -n ${ESXI_USER}@${ESXI_HOST} $cmd | grep -o '[0-9.]\+')
+        # echo $line
+        vmid=$(echo $line | awk '{print $1}')
+        vmname=$(echo $line | awk '{print $2}')
+        cmd="vim-cmd vmsvc/get.summary $vmid | grep ipAddress"
+        echo "cmd: $cmd"
+        ipaddr=$(sshpass -p ${ESXI_PASSWORD} ssh -n ${ESXI_USER}@${ESXI_HOST} $cmd | grep -o '[0-9.]\+')
 
-	echo "Host $vmname
-	Hostname $ipaddr" >> ~/.ssh/config.gen
-	echo -e "${ipaddr}\t${vmname}" >> ~/hosts.tsv
+        if [ -z "$ipaddr" ]; then
+                continue
+        fi
+
+        echo "Host $vmname
+        Hostname $ipaddr" >> ~/.ssh/config.gen
+        echo -e "${ipaddr}\t${vmname}" >> ~/hosts.tsv
 done
 echo "# END_GEN_HOSTS" >> ~/hosts.tsv
